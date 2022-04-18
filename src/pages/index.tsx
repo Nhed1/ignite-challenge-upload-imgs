aefee55d4feb66188c17e2f5e6471fcbd19d68f3
leonardogarreto <leonardogarreto@gmail.com>
2022-04-18 10:50:05 -0300

    started

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,6 +9,9 @@ import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
 export default function Home(): JSX.Element {
+  // function fetchImages({pageParam = null}) {
+  //   return api.get("/api/images?cursor="+pageParam)
+  // }
   const {
     data,
     isLoading,
@@ -18,6 +21,7 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
+
     // TODO AXIOS REQUEST WITH PARAM
     ,
     // TODO GET AND RETURN NEXT PAGE PARAM
